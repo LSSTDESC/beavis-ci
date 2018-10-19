475cf72d3860d3b5b419809c95039ff457832b9d
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-10-18 18:05:40 -0700

    update script

--- beavis-ci.sh
+++ beavis-ci.sh
@@ -16,23 +16,25 @@
 #   repo          The name of a repo to test, eg LSSTDESC/DC2-analysis
 #
 # OPTIONAL INPUTS:
-#   -h --help     Print this header
-#   -a --all      Run all the notebooks in the folder instead of the ones in the README
-#   -u --username GITHUB_USERNAME, defaults to the environment variable
-#   -k --key      GITHUB_API_KEY, defaults to the environment variable
-#   -b --branch   Test the notebooks in a dev branch. Outputs still go to "rendered"
-#   -r --repo     Specify the repo name this way instead
-#   -j --jupyter  Full path to jupyter executable
-#   --kernel      Kernel to have jupyter use
-#   -n --no-push  Only run the notebooks, don't deploy the outputs
-#   --html        Make html outputs instead
+#   -h --help       Print this header
+#   -r --repo       Specify the repo name this way instead
+#   -b --branch     Test the notebooks in a dev branch. Outputs still go to "rendered"
+#   -j --jupyter    Full path to jupyter executable
+#   -n --no-commit  Only run the notebooks, don't commit the outputs
+#   --push          Push the results to rendered
+#   -u --username   GITHUB_USERNAME, defaults to the environment variable
+#   -k --key        GITHUB_API_KEY, defaults to the environment variable
+#   --html          Make html outputs instead
 #
 # OUTPUTS:
 #
 # EXAMPLES:
 #
 # LSST DESC notebooks at NERSC:
-#   ./beavis-ci.sh LSSTDESC/DC2-analysis -u $GITHUB_USERNAME -k $GITHUB_API_KEY --jupyter /usr/common/software/python/3.6-anaconda-4.4/bin/jupyter --kernel desc-stack
+#   ./beavis-ci.sh LSSTDESC/DC2-analysis --jupyter /usr/common/software/python/3.6-anaconda-4.4/bin/jupyter
+#
+# If you have push permission:
+#   ./beavis-ci.sh LSSTDESC/DC2-analysis --push -u $GITHUB_USERNAME -k $GITHUB_API_KEY --jupyter /usr/common/software/python/3.6-anaconda-4.4/bin/jupyter
 #
 #
 # LICENSE:
@@ -74,14 +76,13 @@
 # ======================================================================
 
 HELP=0
-no_push=0
+commit=1
+push=0
 html=0
-all=0
 repo=0
 src="$0"
 branch='master'
 jupyter=$( which jupyter )
-kernel="python"
 
 while [ $# -gt 0 ]; do
     key="$1"
@@ -89,11 +90,11 @@ while [ $# -gt 0 ]; do
         -h|--help)
             HELP=1
             ;;
-        -n|--no-push)
-            no_push=1
+        -n|--no-commit)
+            commit=0
             ;;
-        -a|--all)
-            all=1
+        --push)
+            push=1
             ;;
         -u|--username)
             shift
@@ -118,10 +119,6 @@ while [ $# -gt 0 ]; do
             shift
             jupyter="$1"
             ;;
-        --kernel)
-            shift
-            kernel="$1"
-            ;;
         *)
             repo="$1"
             ;;
@@ -137,7 +134,7 @@ fi
 date
 echo "Welcome to beavis-ci: occasional integration and testing"
 
-if [ $no_push -eq 0 ]; then
+if [ $push -gt 0 ]; then
     if [ -z $GITHUB_USERNAME ] || [ -z $GITHUB_API_KEY ]; then
         echo "No GITHUB_API_KEY and/or GITHUB_USERNAME set, giving up."
         exit 1
@@ -205,8 +202,9 @@ for notebook in $notebooks; do
 
     # Run the notebook:
     $jupyter nbconvert \
-        --ExecutePreprocessor.kernel_name=$kernel \
-        --ExecutePreprocessor.timeout=600 --to $outputformat \
+        --ExecutePreprocessor.allow_errors=True \
+        --ExecutePreprocessor.timeout=1200 \
+        --to $outputformat \
         --execute $filename &> $logfile
 
     cd $workingdir
@@ -221,7 +219,7 @@ for notebook in $notebooks; do
 
 done
 
-if [ $no_push -gt 0 ]; then
+if [ $commit -eq 0 ]; then
     sleep 0
 
 else
@@ -234,11 +232,12 @@ else
     git add -f "${outputs[@]}"
     git add -f "${logs[@]}"
     git commit -m "pushed rendered notebooks and log files"
-    git push -q -f \
-        https://${GITHUB_USERNAME}:${GITHUB_API_KEY}@github.com/${repo} $target
+    if [ $push -gt 0 ]; then
+        git push -q -f \
+            https://${GITHUB_USERNAME}:${GITHUB_API_KEY}@github.com/${repo} $target
+    fi
+    
     echo "Done!"
-    git checkout master
-
     echo ""
     echo "Please read the above output very carefully to see that things are OK. To check we've come back to our starting point correctly, here's a git status:"
     echo ""
@@ -247,8 +246,10 @@ else
 
 fi
 
-echo "beavis-ci finished: view the results at "
-echo "    https://github.com/${repo}/tree/${target}/"
+echo "beavis-ci finished!"
+if [ $push -gt 0 ]; then
+    echo "View results at  https://github.com/${repo}/tree/${target}/"
+fi
 
 cd ../../
 date
